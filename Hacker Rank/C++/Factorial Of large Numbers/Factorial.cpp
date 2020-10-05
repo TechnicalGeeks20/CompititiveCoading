ee47e339d04af8e00e7e47dcaf6e0b54a3038651
Divyam_Sharma <54996898+divyamsharma822@users.noreply.github.com>
2020-10-05 12:43:00 +0530

    Add files via upload

---
+++ Hacker Rank/C++/Factorial Of large Numbers/Factorial.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> factorial(int n){
+    vector<int> fact(1,1);
+    int num;
+    for(int i = 1; i <= n; i++){
+        int carry = 0;
+        for(int j = 0; j < fact.size(); j++){
+            num = i * fact[j] + carry;
+            fact[j] = num%10;
+            carry = num/10;
+        }
+        while(carry){
+            fact.push_back(carry%10);
+            carry /= 10;
+        }
+    }
+    return fact;
+}
+
+
+
+
+int main() {
+	// your code goes here
+	int test,n;
+	
+	cin >> test;
+	
+	while(test--){
+	    cin >> n;
+	    vector<int> largeNum = factorial(n);
+        for(vector<int>::reverse_iterator it = largeNum.rbegin(); it != largeNum.rend();it++){
+            cout << *it;
+        }
+	    cout << endl;
+	}
+	return 0;
+}
